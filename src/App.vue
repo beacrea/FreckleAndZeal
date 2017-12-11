1b960ab06d295fabf4603e7ebe916621e411a9a7
Coty Beasley <coty@cotybeasley.name>
2017-12-10 23:26:22 -0800

    Added an image comparison component

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,17 @@
       <img src="../static/logo.svg" alt="Freckle and Zeal logo" class="logo">
       <h1>{{ heading }}</h1>
       <h2>{{ subheading }}</h2>
-      <p>Give attitude plan steps for world domination so lick the other cats. Sleep nap stretch, forpooping rainbow while flying in a toasted bread costume in space, my left donut is missing, as is my right so purr while eating, yet groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!. Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked! pelt around the house and up and down stairs chasing phantoms so find something else more interesting, so put toy mouse in food bowl run out of litter box at full speed.</p>
-      <p>Ignore the squirrels, you'll never catch them anyway attack feet, for always hungry scamper, andmake meme, make cute face inspect anything brought into the house. Attack feet stare out the window hide at bottom of staircase to trip human put toy mouse in food bowl run out of litter box at full speed . My left donut is missing, as is my right kitty power! . Need to chase tail. Intently sniff hand kitty scratches couch bad kitty yet stare at ceiling, chew iPad power cord, for peer out window, chatter at birds, lure them to mouth so scratch the box. Chase dog then run away i like big cats and i can not lie paw at beetle and eat it before it gets away yet have secret plans.Unwrap toilet paper drink water out of the faucet. Damn that dog chew iPad power cord, andhowl uncontrollably for no reason.</p>
+      <p>Give attitude plan steps for world domination so lick the other cats. Sleep nap stretch, forpooping rainbow while flying in a toasted bread costume in space, my left donut is missing, as is my right so purr while eating, yet groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</p>
+      <ImgComp before="https://placecage.com/1000/600" after="https://placecage.com/g/1000/600" />
     </div>
   </div>
 </template>
 
 <script>
+  import ImgComp from './components/PhotoComparison';
+
   export default {
-    name: 'app',
+    components: {ImgComp},
     data () {
       return {
         heading: 'Hello there!',
--- src/components/PhotoComparison.vue
+++ src/components/PhotoComparison.vue
@@ -1,25 +1,135 @@
 <template>
-  <div class="PhotoComparison">
-    <h1>{{slider_title}}</h1>
-    <input type="range" min="0" max="2" v-bind:value="init_value" class="slide">
-    <div class="labels">
-      <span>{{range1_title}}</span>
-      <span>{{range2_title}}</span>
-      <span>{{range3_title}}</span>
+  <!-- from https://github.com/mhayes/vue-twentytwenty -->
+  <div class="twentytwenty-container"
+       v-bind:style="containerStyle"
+       v-on:touchstart="startSlide"
+       v-on:mousedown="startSlide">
+    <img :src="after" alt="after"
+         v-on:mousedown.prevent
+         v-on:load="setDimensions" />
+    <img :src="before" alt="before"
+         v-on:mousedown.prevent
+         v-bind:style="beforeImgStyle" />
+    <!--<div class="twentytwenty-overlay">
+      <div v-if="beforeLabel" class="twentytwenty-before-label">{{beforeLabel}}</div>
+      <div v-if="afterLabel" class="twentytwenty-after-label">{{afterLabel}}</div>
+    </div>-->
+    <div class="twentytwenty-handle"
+         v-bind:style="handleStyle">
+      <!--<div class="twentytwenty-arrow-left"></div>
+      <div class="twentytwenty-arrow-right"></div>-->
     </div>
   </div>
 </template>
 
 <script>
-  // Import city model
-  import cities from '../store/states'
-
-  // Component logic
   export default {
-    props: ['img1', 'img2'],
     data () {
       return {
+        imgOffset: null,
+        slideOffset: this.offset,
+        sliding: false,
+        containerStyle: {}
       }
+    },
+
+    props: {
+      before: {
+        type: String,
+        required: true
+      },
+      beforeLabel: {
+        type: String
+      },
+      after: {
+        type: String,
+        required: true
+      },
+      afterLabel: {
+        type: String
+      },
+      offset: {
+        type: [String, Number],
+        default: 0.5,
+        validator: (value) => {
+          return (value > 0 && value <= 1)
+        }
+      }
+    },
+
+    methods: {
+      setDimensions () {
+        const img = this.$el.querySelector("img");
+        this.imgOffset = img.getBoundingClientRect();
+        this.containerStyle = { width: `${this.w}px`, height: `${this.h}px` };
+      },
+
+      startSlide (event) {
+        this.sliding = true;
+        this.moveSlide(event)
+      },
+
+      moveSlide (event) {
+        if (this.sliding) {
+          let x = (event.touches ? event.touches[0].pageX : event.pageX) - this.imgOffset.left;
+          x = (x < 0) ? 0 : ((x > this.w) ? this.w : x);
+
+          this.slideOffset = (x / this.w)
+        }
+      },
+
+      endSlide () { this.sliding = false },
+
+      resize () {
+        this.containerStyle = {};
+        this.$nextTick(() => this.setDimensions());
+      }
+    },
+
+    computed: {
+      beforeImgStyle () {
+        return { clip: `rect(0, ${this.x}px, ${this.h}px, 0)` }
+      },
+
+      handleStyle () {
+        const size = 50;
+        return {
+          width: `${size}px`,
+          height: `${size}px`,
+          top:  `calc(50% - ${size/3}px)`,
+          left: `calc(${this.slideOffset*100}% - ${size/2}px)`
+        }
+      },
+
+      x () {
+        return this.w * this.slideOffset
+      },
+
+      w () {
+        if (this.imgOffset)
+          return this.imgOffset.width
+      },
+
+      h () {
+        if (this.imgOffset)
+          return this.imgOffset.height
+      }
+    },
+
+    mounted () {
+      document.addEventListener("touchmove", this.moveSlide)
+      document.addEventListener("touchup", this.endSlide)
+      document.addEventListener("mousemove", this.moveSlide)
+      document.addEventListener("mouseup", this.endSlide)
+      window.addEventListener("resize", this.resize)
+    },
+
+    beforeDestroy () {
+      document.removeEventListener("touchmove", this.moveSlide)
+      document.removeEventListener("touchup", this.endSlide)
+      document.removeEventListener("mousemove", this.moveSlide)
+      document.removeEventListener("mouseup", this.endSlide)
+      window.removeEventListener("resize", this.resize)
     }
   }
 </script>
@@ -28,11 +138,96 @@
   // Imports
   @import "../globalStyles/global";
 
-  // Component Vars
-  $assetsPath: '../../static/';
-
-  // Styles
-  .Slider {
+  .twentytwenty-container {
+    position: relative;
+    overflow: hidden;
+    box-sizing: content-box;
+    border-radius: 6px;
+  }
+  .twentytwenty-container img {
+    max-width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    user-select: none;
+    z-index: 20;
+  }
+  .twentytwenty-container .twentytwenty-overlay {
+    z-index: 25;
     width: 100%;
+    height: 100%;
+    top: 0;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    transition-property: opacity;
+    transition-duration: 0.5s;
+  }
+  .twentytwenty-container .twentytwenty-overlay .twentytwenty-before-label,
+  .twentytwenty-container .twentytwenty-overlay .twentytwenty-after-label {
+    user-select: none;
+    position: absolute;
+    font-size: 0.8em;
+    top: calc(50% - 0.4em - 5px);
+    padding: 10px;
+    background: rgba(255, 255, 255, 0.4);
+    color: white;
+  }
+  .twentytwenty-container .twentytwenty-overlay .twentytwenty-before-label {
+    left: 0;
+  }
+  .twentytwenty-container .twentytwenty-overlay .twentytwenty-after-label {
+    right: 0;
+  }
+  .twentytwenty-container .twentytwenty-overlay:hover {
+    opacity: 1;
+  }
+  .twentytwenty-container .twentytwenty-handle {
+    cursor: move;
+    z-index: 30;
+    position: absolute;
+    border: 4px solid white;
+    border-radius: 50px;
+    margin-left: -2px;
+    margin-top: -4px;
+    user-select: none;
+    background: $color-brand_mainColor_base url('../../static/icon-arrows.png') no-repeat center center;
+    background-size: 90%;
+    box-shadow: 0 0 4px rgba(0,0,0,0.5);
+  }
+  .twentytwenty-container .twentytwenty-handle:before, .twentytwenty-container .twentytwenty-handle:after {
+    content: "";
+    border: 2px solid white;
+    height: 9999px;
+    position: absolute;
+    left: calc(50% - 2px);
+  }
+  .twentytwenty-container .twentytwenty-handle:before {
+    top: 45px;
+  }
+  .twentytwenty-container .twentytwenty-handle:after {
+    bottom: 45px;
+  }
+  .twentytwenty-container .twentytwenty-arrow-right,
+  .twentytwenty-container .twentytwenty-arrow-left {
+    user-select: none;
+    position: relative;
+    width: 0;
+    height: 0;
+  }
+  .twentytwenty-container .twentytwenty-arrow-right {
+    bottom: 10px;
+    left: 23px;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    border-left: 10px solid white;
+  }
+  .twentytwenty-container .twentytwenty-arrow-left {
+    top: 10px;
+    left: 7px;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    border-right: 10px solid white;
   }
 </style>
